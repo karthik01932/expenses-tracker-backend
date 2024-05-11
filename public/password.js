e69c2360632d5bd599e6861d7b7fe108a8e8693a
karthik01932 <113256802+karthik01932@users.noreply.github.com>
2024-05-11 14:43:21 +0530

    added files in cicd

---
+++ public/password.js
@@ -0,0 +1,22 @@
+async function forgotpassword(e) {
+    try {
+        e.preventDefault();
+        console.log(e.target.name);
+        const form = new FormData(e.target);
+    
+        const userDetails = {
+            email: form.get("email"),
+    
+        }
+        
+        console.log(userDetails)
+        const response = await axios.post('http://52.90.203.41:3000/password/forgotpassword',userDetails);
+        if(response.status === 202){
+            document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
+        } else {
+            throw new Error('Something went wrong!!!')
+        }
+    } catch (error) {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    }
+}
